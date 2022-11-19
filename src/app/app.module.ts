c03f5b8cf81df476268417265098ef6097d302de
Rafael Araujo Chinaglia <chinaglia.rafa@gmail.com>
2022-11-19 19:40:29 -0300

    ímplementa recursos básicos e telas iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaperButtonComponent } from './component/paper-button/paper-button.component';
+import { CharacterCreatorComponent } from './pages/character-creator/character-creator.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaperButtonComponent,
+    CharacterCreatorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/paper-button/paper-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'paper-button',
+  templateUrl: './paper-button.component.html',
+  styleUrls: ['./paper-button.component.scss'],
+})
+export class PaperButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
